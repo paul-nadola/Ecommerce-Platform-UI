31faf764048baff7f2eb8e7ad6ec5ef24c471d54
Paul Nadola Oyinga <nadolapaul@gmail.com>
2024-03-22 09:38:00 +0300

    added auth service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserService } from '../_services/user.service';
+import { UserAuthService } from '../_services/user-auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,8 @@ import { UserService } from '../_services/user.service';
 export class LoginComponent implements OnInit {
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private userAuthService: UserAuthService
   ) {}
 
   ngOnInit():void {};
