810394622a0fe29cfc57bc2e7fb8d997b8fbb0d8
Paul Nadola Oyinga <nadolapaul@gmail.com>
2024-04-22 16:03:18 +0300

    availing products to every iuser

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../_services/product.service';
+import { map } from 'rxjs';
+import { Product } from '../_model/product.model';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ImageProcessingService } from '../image-processing.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +11,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  productDetails: Product[] = [];
+  constructor (
+    private productService: ProductService,
+    private imageProcessingService: ImageProcessingService
+  ) {
 
+  }
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  public getAllProducts() {
+    this.productService.getAllProducts()
+      .pipe(
+        map((x: Product[], i: any) => x.map((product: Product) => this.imageProcessingService.createImages(product)))
+      )
+      .subscribe((res: Product[]) => {
+        console.log(res);
+        this.productDetails = res;
+      }, (error: HttpErrorResponse) => {
+        console.log(error)
+      }
+      )
+
+  }
 }
